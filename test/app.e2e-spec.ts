d1d8f7609bfe7872e888ccf3ee0469edd2ebded1
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-05-31 01:57:22 +0200

    feat: add initial tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,102 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { print } from 'graphql';
+import { SignUp } from 'src/schema-nodes';
+import { AppModule } from 'src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3333);
+
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  const userOne = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'jhondoe@gmail.com',
+    password: '123456',
+  };
+
+  describe('Authentication', () => {
+    describe('SignUp', () => {
+      it('Should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              firstName: userOne.firstName,
+              lastName: userOne.lastName,
+              password: userOne.password,
+            },
+          })
+          .expectStatus(400);
+      });
+
+      it('Should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              firstName: userOne.firstName,
+              lastName: userOne.lastName,
+              email: userOne.email,
+            },
+          })
+          .expectStatus(400);
+      });
+
+      it('Should throw if firstName empty', () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              lastName: userOne.lastName,
+              email: userOne.email,
+              password: userOne.password,
+            },
+          })
+          .expectStatus(400);
+      });
+
+      it(`Should create new user: ${userOne.firstName} ${userOne.lastName}`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              firstName: userOne.firstName,
+              lastName: userOne.lastName,
+              email: userOne.email,
+              password: userOne.password,
+            },
+          })
+          .inspect();
+      });
+    });
   });
 });
