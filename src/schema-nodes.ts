20cfd46175243aecd6d7af95a2662b2e07f58e09
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-06-05 01:41:25 +0200

    chore: update tests

--- src/schema-nodes.ts
+++ src/schema-nodes.ts
@@ -1,5 +1,36 @@
 import gql from 'graphql-tag';
 
+export const ChangePassword = gql`
+  mutation changePassword($input: ChangePasswordType!) {
+    changePassword(input: $input)
+  }
+`;
+export const CreateIdea = gql`
+  mutation createIdea($input: CreateIdeaType!) {
+    createIdea(input: $input) {
+      authorId
+      content
+      createdAt
+      id
+      visibility
+    }
+  }
+`;
+export const DeleteIdea = gql`
+  mutation deleteIdea($input: DeleteIdeaType!) {
+    deleteIdea(input: $input)
+  }
+`;
+export const RequestResetPassword = gql`
+  mutation requestResetPassword($input: RequestResetPassowrdType!) {
+    requestResetPassword(input: $input)
+  }
+`;
+export const ResetPassword = gql`
+  mutation resetPassword($input: ResetPasswordType!) {
+    resetPassword(input: $input)
+  }
+`;
 export const SignIn = gql`
   mutation signIn($input: SignInType!) {
     signIn(input: $input) {
@@ -19,6 +50,40 @@ export const SignUp = gql`
       id
       lastName
       token
+    }
+  }
+`;
+export const UpdateIdea = gql`
+  mutation updateIdea($input: UpdateIdeaType!) {
+    updateIdea(input: $input) {
+      authorId
+      content
+      createdAt
+      id
+      visibility
+    }
+  }
+`;
+export const ListIdeas = gql`
+  query listIdeas($input: ListIdeaType!) {
+    listIdeas(input: $input) {
+      edges {
+        cursor
+        node {
+          authorId
+          content
+          createdAt
+          id
+          visibility
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      totalCount
     }
   }
 `;
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as pactum from 'pactum';
 import { print } from 'graphql';
-import { SignUp } from 'src/schema-nodes';
+import { Me, SignIn, SignUp } from 'src/schema-nodes';
 import { AppModule } from 'src/app.module';
 
 describe('App e2e', () => {
@@ -29,15 +29,29 @@ describe('App e2e', () => {
   });
 
   const userOne = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'jhondoe@gmail.com',
+    firstName: 'Marty',
+    lastName: 'McFly',
+    email: 'martymcfly@gmail.com',
     password: '123456',
+  };
+
+  const userTwo = {
+    firstName: 'Emmett',
+    lastName: 'Brown',
+    email: 'emmettbrown@gmail.com',
+    password: 'password1234',
+  };
+
+  const userThree = {
+    firstName: 'Lorraine',
+    lastName: 'Baines',
+    email: 'lorrainebaines@gmail.com',
+    password: 'pass22222',
   };
 
   describe('Authentication', () => {
     describe('SignUp', () => {
-      it('Should throw if email empty', () => {
+      it('Should throw if email is empty', () => {
         return pactum
           .spec()
           .post('/graphql')
@@ -49,10 +63,18 @@ describe('App e2e', () => {
               password: userOne.password,
             },
           })
-          .expectStatus(400);
+          .expectJsonLike({
+            errors: [
+              {
+                extensions: {
+                  code: 'BAD_USER_INPUT',
+                },
+              },
+            ],
+          });
       });
 
-      it('Should throw if password empty', () => {
+      it('Should throw if password is empty', () => {
         return pactum
           .spec()
           .post('/graphql')
@@ -64,10 +86,18 @@ describe('App e2e', () => {
               email: userOne.email,
             },
           })
-          .expectStatus(400);
+          .expectJsonLike({
+            errors: [
+              {
+                extensions: {
+                  code: 'BAD_USER_INPUT',
+                },
+              },
+            ],
+          });
       });
 
-      it('Should throw if firstName empty', () => {
+      it('Should throw if firstName is empty', () => {
         return pactum
           .spec()
           .post('/graphql')
@@ -79,7 +109,15 @@ describe('App e2e', () => {
               password: userOne.password,
             },
           })
-          .expectStatus(400);
+          .expectJsonLike({
+            errors: [
+              {
+                extensions: {
+                  code: 'BAD_USER_INPUT',
+                },
+              },
+            ],
+          });
       });
 
       it(`Should create new user: ${userOne.firstName} ${userOne.lastName}`, () => {
@@ -95,7 +133,218 @@ describe('App e2e', () => {
               password: userOne.password,
             },
           })
-          .inspect();
+          .expectJsonLike({
+            data: {
+              signUp: {
+                email: userOne.email,
+                firstName: userOne.firstName,
+                lastName: userOne.lastName,
+              },
+            },
+          });
+      });
+
+      it(`Should throw if creates again: ${userOne.firstName} ${userOne.lastName}`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              firstName: userOne.firstName,
+              lastName: userOne.lastName,
+              email: userOne.email,
+              password: userOne.password,
+            },
+          })
+          .expectJsonLike({
+            errors: [
+              {
+                message: 'User already exists',
+              },
+            ],
+          });
+      });
+
+      it(`Should create new user: ${userTwo.firstName} ${userTwo.lastName}`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              firstName: userTwo.firstName,
+              lastName: userTwo.lastName,
+              email: userTwo.email,
+              password: userTwo.password,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              signUp: {
+                email: userTwo.email,
+                firstName: userTwo.firstName,
+                lastName: userTwo.lastName,
+              },
+            },
+          });
+      });
+
+      it(`Should create new user: ${userThree.firstName} ${userThree.lastName}`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignUp))
+          .withGraphQLVariables({
+            input: {
+              firstName: userThree.firstName,
+              lastName: userThree.lastName,
+              email: userThree.email,
+              password: userThree.password,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              signUp: {
+                email: userThree.email,
+                firstName: userThree.firstName,
+                lastName: userThree.lastName,
+              },
+            },
+          });
+      });
+    });
+
+    describe('SignIn', () => {
+      it(`Login ${userOne.firstName} ${userOne.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignIn))
+          .withGraphQLVariables({
+            input: {
+              email: userOne.email,
+              password: userOne.password,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              signIn: {
+                email: userOne.email,
+                firstName: userOne.firstName,
+                lastName: userOne.lastName,
+              },
+            },
+          })
+          .stores('userOneToken', 'data.signIn.token');
+      });
+
+      it(`Login ${userTwo.firstName} ${userTwo.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignIn))
+          .withGraphQLVariables({
+            input: {
+              email: userTwo.email,
+              password: userTwo.password,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              signIn: {
+                email: userTwo.email,
+                firstName: userTwo.firstName,
+                lastName: userTwo.lastName,
+              },
+            },
+          })
+          .inspect()
+          .stores('userTwoToken', 'data.signIn.token');
+      });
+
+      it(`Login ${userThree.firstName} ${userThree.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withGraphQLQuery(print(SignIn))
+          .withGraphQLVariables({
+            input: {
+              email: userThree.email,
+              password: userThree.password,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              signIn: {
+                email: userThree.email,
+                firstName: userThree.firstName,
+                lastName: userThree.lastName,
+              },
+            },
+          })
+          .stores('userThreeToken', 'data.signIn.token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Me', () => {
+      it(`Get ${userOne.firstName} ${userOne.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(Me))
+          .expectJsonLike({
+            data: {
+              me: {
+                email: userOne.email,
+                firstName: userOne.firstName,
+                lastName: userOne.lastName,
+              },
+            },
+          });
+      });
+
+      it(`Get ${userTwo.firstName} ${userTwo.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userTwoToken}',
+          })
+          .withGraphQLQuery(print(Me))
+          .expectJsonLike({
+            data: {
+              me: {
+                email: userTwo.email,
+                firstName: userTwo.firstName,
+                lastName: userTwo.lastName,
+              },
+            },
+          });
+      });
+
+      it(`Get ${userThree.firstName} ${userThree.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userThreeToken}',
+          })
+          .withGraphQLQuery(print(Me))
+          .expectJsonLike({
+            data: {
+              me: {
+                email: userThree.email,
+                firstName: userThree.firstName,
+                lastName: userThree.lastName,
+              },
+            },
+          });
       });
     });
   });
