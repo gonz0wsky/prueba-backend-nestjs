484154fc29b80e55fdac1d90e85e1f2c141d6fcc
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-06-05 14:27:15 +0200

    chore: add idea tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,13 +4,18 @@ import * as pactum from 'pactum';
 import { print } from 'graphql';
 import {
   ChangePassword,
+  CreateIdea,
+  DeleteIdea,
+  ListIdeas,
   Me,
   RequestResetPassword,
   ResetPassword,
   SignIn,
   SignUp,
+  UpdateIdea,
 } from 'src/schema-nodes';
 import { AppModule } from 'src/app.module';
+import { BAD_USER_INPUT } from './commonResponses';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -70,15 +75,7 @@ describe('App e2e', () => {
               password: userOne.password,
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it('Should throw if password is empty', () => {
@@ -93,15 +90,7 @@ describe('App e2e', () => {
               email: userOne.email,
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it('Should throw if firstName is empty', () => {
@@ -116,15 +105,7 @@ describe('App e2e', () => {
               password: userOne.password,
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it(`Should create new user: ${userOne.firstName} ${userOne.lastName}`, () => {
@@ -266,7 +247,6 @@ describe('App e2e', () => {
               },
             },
           })
-          .inspect()
           .stores('userTwoToken', 'data.signIn.token');
       });
 
@@ -308,15 +288,7 @@ describe('App e2e', () => {
               newPassword: userOne.password,
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it(`Should throw if newPassrod is empty`, () => {
@@ -332,15 +304,7 @@ describe('App e2e', () => {
               newPassword: userOne.password,
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it(`Should fail if wrong password ${userOne.firstName} ${userOne.lastName} user`, () => {
@@ -397,15 +361,7 @@ describe('App e2e', () => {
           .withGraphQLVariables({
             input: {},
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it(`Request reset password ${userOne.firstName} ${userOne.lastName} user`, () => {
@@ -436,15 +392,7 @@ describe('App e2e', () => {
               password: userOne.password,
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it(`Should throw if password is empty`, () => {
@@ -457,15 +405,7 @@ describe('App e2e', () => {
               token: '$S{userOneResetPasswordToken}',
             },
           })
-          .expectJsonLike({
-            errors: [
-              {
-                extensions: {
-                  code: 'BAD_USER_INPUT',
-                },
-              },
-            ],
-          });
+          .expectJsonLike(BAD_USER_INPUT);
       });
 
       it(`Reset password ${userOne.firstName} ${userOne.lastName} user`, () => {
@@ -543,6 +483,394 @@ describe('App e2e', () => {
                 firstName: userThree.firstName,
                 lastName: userThree.lastName,
               },
+            },
+          });
+      });
+    });
+  });
+
+  describe('Idea', () => {
+    describe('Create', () => {
+      it(`Should throw if empty content`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(CreateIdea))
+          .withGraphQLVariables({
+            input: {
+              visibility: 'PUBLIC',
+            },
+          })
+          .expectJsonLike(BAD_USER_INPUT);
+      });
+
+      it(`Should throw if empty visibility`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(CreateIdea))
+          .withGraphQLVariables({
+            input: {
+              content: "I'm an idea",
+            },
+          })
+          .expectJsonLike(BAD_USER_INPUT);
+      });
+
+      it(`Should throw if not valid visibility value`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(CreateIdea))
+          .withGraphQLVariables({
+            input: {
+              content: "I'm an idea",
+              visibility: 'PUBLIC_PRIVATE',
+            },
+          })
+          .expectJsonLike(BAD_USER_INPUT);
+      });
+
+      it(`Create an idea with ${userOne.firstName} ${userOne.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(CreateIdea))
+          .withGraphQLVariables({
+            input: {
+              content: "I'm an idea",
+              visibility: 'PUBLIC',
+            },
+          })
+          .expectJsonLike({
+            data: {
+              createIdea: {
+                content: "I'm an idea",
+                visibility: 'PUBLIC',
+              },
+            },
+          })
+          .stores('ideaOneId', 'data.createIdea.id');
+      });
+
+      it(`Create an idea with ${userTwo.firstName} ${userTwo.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userTwoToken}',
+          })
+          .withGraphQLQuery(print(CreateIdea))
+          .withGraphQLVariables({
+            input: {
+              content: "I'm an idea 2",
+              visibility: 'PROTECTED',
+            },
+          })
+          .expectJsonLike({
+            data: {
+              createIdea: {
+                content: "I'm an idea 2",
+                visibility: 'PROTECTED',
+              },
+            },
+          })
+          .stores('ideaTwoId', 'data.createIdea.id');
+      });
+
+      it(`Create an idea with ${userThree.firstName} ${userThree.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userThreeToken}',
+          })
+          .withGraphQLQuery(print(CreateIdea))
+          .withGraphQLVariables({
+            input: {
+              content: "I'm an idea 3",
+              visibility: 'PRIVATE',
+            },
+          })
+          .expectJsonLike({
+            data: {
+              createIdea: {
+                content: "I'm an idea 3",
+                visibility: 'PRIVATE',
+              },
+            },
+          })
+          .stores('ideaThreeId', 'data.createIdea.id');
+      });
+    });
+
+    describe('Update', () => {
+      it(`Should throw if id is empty`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(UpdateIdea))
+          .withGraphQLVariables({
+            input: {
+              content: "I'm an idea 1",
+              visibility: 'PROTECTED',
+            },
+          })
+          .expectJsonLike(BAD_USER_INPUT);
+      });
+
+      it(`Should throw if not valid visibility`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(UpdateIdea))
+          .withGraphQLVariables({
+            input: {
+              id: '$S{ideaOneId}',
+              visibility: 'PROTECTED_PUBLIC',
+            },
+          })
+          .expectJsonLike(BAD_USER_INPUT);
+      });
+
+      it(`Update idea from ${userOne.firstName} ${userOne.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(UpdateIdea))
+          .withGraphQLVariables({
+            input: {
+              id: '$S{ideaOneId}',
+              content: "I'm an idea 1",
+              visibility: 'PROTECTED',
+            },
+          })
+          .expectJsonLike({
+            data: {
+              updateIdea: {
+                content: "I'm an idea 1",
+                visibility: 'PROTECTED',
+              },
+            },
+          });
+      });
+
+      it(`Restore idea from ${userOne.firstName} ${userOne.lastName} user to PUBLIC`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(UpdateIdea))
+          .withGraphQLVariables({
+            input: {
+              id: '$S{ideaOneId}',
+              visibility: 'PUBLIC',
+            },
+          })
+          .expectJsonLike({
+            data: {
+              updateIdea: {
+                visibility: 'PUBLIC',
+              },
+            },
+          });
+      });
+    });
+
+    describe('List', () => {
+      it(`List ideas from ${userOne.firstName} ${userOne.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(ListIdeas))
+          .withGraphQLVariables({
+            input: {
+              first: 5,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              listIdeas: {
+                edges: [
+                  {
+                    node: {
+                      content: "I'm an idea 1",
+                    },
+                  },
+                ],
+              },
+            },
+          });
+      });
+
+      it(`List ideas from ${userTwo.firstName} ${userTwo.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userTwoToken}',
+          })
+          .withGraphQLQuery(print(ListIdeas))
+          .withGraphQLVariables({
+            input: {
+              first: 5,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              listIdeas: {
+                edges: [
+                  {
+                    node: {
+                      content: "I'm an idea 2",
+                    },
+                  },
+                ],
+              },
+            },
+          });
+      });
+
+      it(`List ideas from ${userThree.firstName} ${userThree.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userThreeToken}',
+          })
+          .withGraphQLQuery(print(ListIdeas))
+          .withGraphQLVariables({
+            input: {
+              first: 5,
+            },
+          })
+          .expectJsonLike({
+            data: {
+              listIdeas: {
+                edges: [
+                  {
+                    node: {
+                      content: "I'm an idea 3",
+                    },
+                  },
+                ],
+              },
+            },
+          });
+      });
+    });
+
+    describe('Delete', () => {
+      it(`Should throw if id is empty`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(DeleteIdea))
+          .withGraphQLVariables({
+            input: {},
+          })
+          .expectJsonLike(BAD_USER_INPUT);
+      });
+
+      it(`Should throw if id is not valid`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(DeleteIdea))
+          .withGraphQLVariables({
+            input: { id: 'not_valid_token' },
+          })
+          .expectJsonLike({
+            errors: [
+              {
+                message: 'Idea not found',
+              },
+            ],
+          });
+      });
+
+      it(`Delete idea from ${userOne.firstName} ${userOne.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userOneToken}',
+          })
+          .withGraphQLQuery(print(DeleteIdea))
+          .withGraphQLVariables({
+            input: { id: '$S{ideaOneId}' },
+          })
+          .expectJsonLike({
+            data: {
+              deleteIdea: true,
+            },
+          });
+      });
+
+      it(`Delete idea from ${userTwo.firstName} ${userTwo.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userTwoToken}',
+          })
+          .withGraphQLQuery(print(DeleteIdea))
+          .withGraphQLVariables({
+            input: { id: '$S{ideaTwoId}' },
+          })
+          .expectJsonLike({
+            data: {
+              deleteIdea: true,
+            },
+          });
+      });
+
+      it(`Delete idea from ${userThree.firstName} ${userThree.lastName} user`, () => {
+        return pactum
+          .spec()
+          .post('/graphql')
+          .withHeaders({
+            Authorization: 'Bearer $S{userThreeToken}',
+          })
+          .withGraphQLQuery(print(DeleteIdea))
+          .withGraphQLVariables({
+            input: { id: '$S{ideaThreeId}' },
+          })
+          .expectJsonLike({
+            data: {
+              deleteIdea: true,
             },
           });
       });
---
+++ test/commonResponses.ts
@@ -0,0 +1,9 @@
+export const BAD_USER_INPUT = {
+  errors: [
+    {
+      extensions: {
+        code: 'BAD_USER_INPUT',
+      },
+    },
+  ],
+};
